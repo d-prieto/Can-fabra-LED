1983d91101d6dd9969f2def90b8e01577ba71d17
Pol <159940382+Polivet0815@users.noreply.github.com>
2025-10-07 19:56:40 +0200

    Update Persigue el queso.

--- pol/Persigue el queso.ino
+++ pol/Persigue el queso.ino
@@ -5,7 +5,8 @@ CRGB leds[NUM_LEDS];
 int ledActual = 1; //led central
 bool pAlante = true;
 int estadoEjeY=analogRead(A0);
-
+int queso=random(4,NUM_LEDS);
+int Puntos=0;
 void setup() {
    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
    Serial.begin(9600);
@@ -15,20 +16,45 @@ void setup() {
 void loop() {
   Serial.println(estadoEjeY);
   //leds[] = CRGB::Purple;
-  
+  ComerQueso();
    estadoEjeY = analogRead(A0);
   FastLED.clear();
+  leds[queso] = CRGB::Yellow;
   leds[ledActual] = CRGB::Purple;
   leds[ledActual +1] = CRGB::Purple;
   leds[ledActual -1] = CRGB::Purple;
   delay(50);
   FastLED.show();
-  if (estadoEjeY<312) {
+  if (estadoEjeY<112) {
     ledActual--;
     
   }
-  else {
+  if(estadoEjeY<312) {
+    ledActual --;
+   
+    }
+    if(estadoEjeY>612) {
+    ledActual ++;
+   
+    }
+    if(estadoEjeY>812) {
     ledActual ++;
    
     }
   }
+
+
+void ComerQueso(){
+
+  if(queso==ledActual){
+  MostrarPuntos();
+  queso=random(4,NUM_LEDS);
+}
+
+
+void MostrarPuntos(){
+  FastLED.clear();
+  for(int a=0;a=Puntos;a++){
+     leds[a] = CRGB::Yellow; 
+  }
+}
